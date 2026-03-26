795b11ccd54a3bc1fc2c675cc63b43e4ae2b0579
Fredrik Sagen <fredrik.a.sagen@gmail.com>
2026-03-26 12:35:41 +0100

    Initial commit

---
+++ 01 Programming Foundations/AW01 - Module 1. Variables, Alerts and debugging, Conditionals/1.2. Variables, operators, and Boolean logic/Activity-1/activity.js
@@ -0,0 +1,6 @@
+let x = 32; // This is a valid variable declaration and assignment. The variable "x" is assigned the value of 32.
+let userScore = 18;// This is a valid variable declaration and assignment. The variable "userScore" is assigned the value of 18.
+//let 3cats = "Whiskers"; // This line will cause an error because variable names cannot start with a number. Variable names must begin with a letter, underscore (_), or dollar sign ($).
+let thing = "item"; // This is a valid variable declaration and assignment. The variable "thing" is assigned the value of "item".
+let API_KEY = "123abc"; // This is a valid variable declaration and assignment. The variable "API_KEY" is assigned the value of "123abc". It is common practice to use uppercase letters for constants, but it is not a requirement.
+//let const = true; // This line will cause an error because "const" is a reserved keyword in JavaScript and cannot be used as a variable name.
---
+++ 01 Programming Foundations/AW01 - Module 1. Variables, Alerts and debugging, Conditionals/1.2. Variables, operators, and Boolean logic/Activity-2/js.js
@@ -0,0 +1,11 @@
+let result = 10 - 2 * 3 + 4;
+console.log(result); // 8
+
+let result1 = (10 - 2) * 3 + 4;
+console.log(result1); // 28
+
+let result2 = 10 - 2 * (3 + 4);
+console.log(result2); // -4
+
+let result3 = (10 - 2) * (3 + 4);
+console.log(result3); // 56
